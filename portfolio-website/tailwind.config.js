c9d6cbef72f0eb1c8631a9b89f424846ecbcca72
Sherika <faysons81@gmail.com>
2024-03-18 18:30:19 -0400

    Base Styling and Theme Setup

--- portfolio-website/tailwind.config.js
+++ portfolio-website/tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          brightBlue: '#3AAED8',
+          lightTurquoise: '#7CFEF0',
+          mintGreen: '#D7F9F1',
+          deepGreen: '#618B4A',
+          vibrantGreen: '#28965A',
+          sageGreen: '#629460',
+        },
+        text: {
+          lightMode: '#333333',
+          darkMode: '#F5F5F5',
+        },
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+        'crimson-text': ['Crimson Text', 'serif'],
+      },
+    },
   },
-  plugins: [],
-}
-
+};
